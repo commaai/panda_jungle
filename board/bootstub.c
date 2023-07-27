d33a1b9320099f7889d66c28375cd1912d4b5452
Robbe Derks <robbe.derks@gmail.com>
2023-07-27 15:33:43 -0700

    keep power on when in bootstub

--- board/bootstub.c
+++ board/bootstub.c
@@ -40,6 +40,7 @@ int main(void) {
   disable_interrupts();
   clock_init();
   detect_board_type();
+  current_board->set_panda_power(true);
 
   if (enter_bootloader_mode == ENTER_SOFTLOADER_MAGIC) {
     enter_bootloader_mode = 0;
